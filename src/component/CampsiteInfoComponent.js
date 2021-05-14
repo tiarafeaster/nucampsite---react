36dd8ca8a392c62c8f4f1aad6a9573ae234df2a6
Tiara Feaster <tfeaster21@gmail.com>
2021-05-14 15:22:05 -0400

    updated package.json

---
+++ src/component/CampsiteInfoComponent.js
@@ -0,0 +1,226 @@
+import React, { Component } from "react";
+import {
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	Breadcrumb,
+	BreadcrumbItem,
+	Button,
+	Modal,
+	ModalHeader,
+	ModalBody,
+	Label,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isModalopen: false,
+			rating: "",
+			author: "",
+			text: "",
+		};
+	}
+
+	toggleModal = () => {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	};
+
+	handleSubmit = (values) => {
+		this.toggleModal();
+		this.props.postComment(
+			this.props.campsiteId,
+			values.rating,
+			values.author,
+			values.text
+		);
+	};
+
+	render() {
+		return (
+			<>
+				<Button outline onClick={this.toggleModal}>
+					<i className="fa fa-pencil fa-lg" />
+					Submit Comment{" "}
+				</Button>
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+							<div className="form-group">
+								<Label htmlFor="rating">Rating</Label>
+								<Control.select
+									className="form-control"
+									model=".rating"
+									id="rating"
+									defaultValue="5"
+								>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option selected>5</option>
+								</Control.select>
+							</div>
+							<div className="form-group">
+								<Label htmlFor="author">Author</Label>
+								<Control.text
+									className="form-control"
+									model=".author"
+									name="author"
+									id="author"
+									placeholder="Your Name"
+									validators={{
+										required,
+										minLength: minLength(2),
+										maxLength: maxLength(15),
+									}}
+								/>
+								<Errors
+									className="text-danger"
+									model=".author"
+									show="touched"
+									component="div"
+									messages={{
+										required: "Required",
+										minLength: "Must be at least 2 characters",
+										maxLength: "Must be 15 characters or less",
+									}}
+								/>
+							</div>
+							<div className="form-group">
+								<Label htmlFor="text">Comment</Label>
+								<Control.textarea
+									className="form-control"
+									model=".text"
+									name="text"
+									id="text"
+									rows="6"
+								/>
+							</div>
+							<Button type="submit" value="submit" color="primary">
+								Submit
+							</Button>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</>
+		);
+	}
+}
+
+function RenderCampsite({ campsite }) {
+	return (
+		<div className="col-md-5 m-1">
+			<FadeTransform
+				in
+				transformProps={{
+					exitTransform: "scale(0.5) translateY(50%)",
+				}}
+			>
+				<Card>
+					<CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+					<CardBody>
+						<CardText>{campsite.description}</CardText>
+					</CardBody>
+				</Card>
+			</FadeTransform>
+		</div>
+	);
+}
+
+function RenderComments({ comments, postComment, campsiteId }) {
+	if (comments) {
+		return (
+			<div className="col-md-5 m-1">
+				<h4>Comments</h4>
+				<Stagger in>
+					{comments.map((comments) => (
+						<Fade in key={comments.id}>
+							<div className="p-1">
+								{comments.text}
+								<br />
+								-- {comments.author},{" "}
+								{new Intl.DateTimeFormat("en-US", {
+									year: "numeric",
+									month: "short",
+									day: "2-digit",
+								}).format(new Date(Date.parse(comments.date)))}
+							</div>
+						</Fade>
+					))}
+				</Stagger>
+				<CommentForm campsiteId={campsiteId} postComment={postComment} />
+			</div>
+		);
+	}
+	return <div />;
+}
+
+function CampsiteInfo(props) {
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	if (props.errMess) {
+		return (
+			<div classname="container">
+				<div className="row">
+					<div className="col">
+						<h4>{props.errMess}</h4>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	if (props.campsite) {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<Breadcrumb>
+							<BreadcrumbItem>
+								<Link to="/directory">Directory</Link>{" "}
+							</BreadcrumbItem>
+							<BreadcrumbItem>
+								<Link active>{props.campsite.name}</Link>
+							</BreadcrumbItem>
+						</Breadcrumb>
+						<h2>{props.campsite.name}</h2>
+						<hr />
+					</div>
+				</div>
+				<div className="row">
+					<RenderCampsite campsite={props.campsite} />
+					<RenderComments
+						comments={props.comments}
+						postComment={props.postComment}
+						campsiteId={props.campsite.id}
+					/>
+				</div>
+			</div>
+		);
+	}
+	return <div />;
+}
+
+export default CampsiteInfo;
